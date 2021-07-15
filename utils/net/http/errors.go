d3bbcba935af5dfb6fb5f6e84e9883f54e9372a9
Frederik Löffert <frederik@bestbytes.com>
2021-07-15 11:32:16 +0200

    feat: added not found helper to httputils

--- utils/net/http/errors.go
+++ utils/net/http/errors.go
@@ -23,6 +23,11 @@ func BadRequestServerError(l *zap.Logger, w http.ResponseWriter, r *http.Request
 	ServerError(l, w, r, http.StatusBadRequest, err)
 }
 
+// NotFoundServerError http response
+func NotFoundServerError(l *zap.Logger, w http.ResponseWriter, r *http.Request, err error) {
+	ServerError(l, w, r, http.StatusNotFound, err)
+}
+
 // ServerError http response
 func ServerError(l *zap.Logger, w http.ResponseWriter, r *http.Request, code int, err error) {
 	if err != nil {
