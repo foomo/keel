1521eb1538af123f5ed1dddb22b7e3cb3b932131
botastic <mrbotastic@gmail.com>
2021-08-17 14:36:24 +0200

    added bcrypt based basic authentication

--- net/http/middleware/auth.go
+++ net/http/middleware/auth.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
 
 	"github.com/foomo/keel/log"
 )
@@ -43,6 +44,31 @@ func BasicAuth(user, password string) Middleware {
 
 			// This is a dummy check for credentials.
 			if u != user || p != password {
+				unauthorised(rw)
+				return
+			}
+
+			// If required, Context could be updated to include authentication
+			// related data so that it could be used in consequent steps.
+			next.ServeHTTP(rw, rq)
+		})
+	}
+}
+
+func BasicAuthBcryptHash(hashedUser, hashedPassword string) Middleware {
+	return func(l *zap.Logger, next http.Handler) http.Handler {
+		return http.HandlerFunc(func(rw http.ResponseWriter, rq *http.Request) {
+
+			u, p, ok := rq.BasicAuth()
+			if !ok || len(strings.TrimSpace(u)) < 1 || len(strings.TrimSpace(p)) < 1 {
+				unauthorised(rw)
+				return
+			}
+
+			// Compare the username and password hash with the ones in the request
+			errU := bcrypt.CompareHashAndPassword([]byte(hashedUser), []byte(u))
+			errP := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(p))
+			if errU != nil || errP != nil {
 				unauthorised(rw)
 				return
 			}
