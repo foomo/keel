a0eb6829cd6dd651a95bdacfe65bfb162ed8b62d
franklin <kevin@franklinkim.de>
2021-09-06 22:03:13 +0200

    style: fix format

--- net/http/middleware/middleware.go
+++ net/http/middleware/middleware.go
@@ -7,9 +7,7 @@ import (
 )
 
 // Middleware your way to handle requests
-type (
-	Middleware func(*zap.Logger, http.Handler) http.Handler
-)
+type Middleware func(*zap.Logger, http.Handler) http.Handler
 
 func Compose(l *zap.Logger, handler http.Handler, middlewares ...Middleware) http.Handler {
 	composed := func(l *zap.Logger, next http.Handler) http.Handler {
