ef9575a329bbc1c0a32612615755f7b0e113d960
franklin <kevin@franklinkim.de>
2021-09-06 22:09:58 +0200

    feat: add is

--- net/gotsrpc/errors.go
+++ net/gotsrpc/errors.go
@@ -15,6 +15,16 @@ func NewError(e Error) *Error {
 	return &e
 }
 
+// Is interface
+func (e *Error) Is(err error) bool {
+	if e == nil || err == nil {
+		return false
+	} else if v, ok := err.(*Error); ok && v != nil {
+		return e.Error() == v.Error()
+	}
+	return false
+}
+
 // Error interface
 func (e *Error) Error() string {
 	return string(*e)
