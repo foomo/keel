dc40e57da3fcb2fedbfd7ac0234ef87b0d8890ea
Kevin Franklin Kim <franklin@weareinteractive.com>
2021-11-19 12:24:41 +0100

    feat: allow nil

--- test/log.go
+++ test/log.go
@@ -2,11 +2,20 @@ package keeltest
 
 import (
 	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 	"go.uber.org/zap/zaptest"
 )
 
 func Logger(t zaptest.TestingT) *zap.Logger {
-	l := zaptest.NewLogger(t)
+	var l *zap.Logger
+	if t == nil {
+		c := zap.NewDevelopmentConfig()
+		c.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+		c.EncoderConfig.TimeKey = ""
+		l, _ = c.Build()
+	} else {
+		l = zaptest.NewLogger(t)
+	}
 	// setup logger
 	zap.ReplaceGlobals(l)
 	return l
