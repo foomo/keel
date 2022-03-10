4917c96c4665f4599f73ea7a19d02638b13b487e
Ognjen <ognjen.vladisavljevic@bestbytes.com>
2022-03-10 15:31:47 +0100

    chore: add docs

--- probetype.go
+++ probetype.go
@@ -5,9 +5,13 @@ type ProbeType string
 const probesServiceName = "probes"
 
 const (
-	ProbeTypeAll        ProbeType = "all"
-	ProbeTypeStartup    ProbeType = "startup"
-	ProbeTypeReadiness  ProbeType = "readiness"
+	ProbeTypeAll ProbeType = "all"
+	// https://kubernetes.io/docs/tasks/configure-pod-container/configure-liveness-readiness-startup-probes/
+	// Startup probe disables liveness and readiness checks until it succeeds
+	ProbeTypeStartup ProbeType = "startup"
+	// Kubernetes uses readiness probes to know when a container is ready to start accepting traffic
+	ProbeTypeReadiness ProbeType = "readiness"
+	// Kubernetes uses liveness probes to know when to restart a container
 	ProbeTypeLiveliness ProbeType = "liveliness"
 )
 
