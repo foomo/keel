07238195cd22e0368f76b52cdbcf3d959dcd1637
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2022-03-14 10:42:29 +0100

    feat: add dump roundtripware

--- net/http/roundtripware/dump.go
+++ net/http/roundtripware/dump.go
@@ -7,16 +7,23 @@ import (
 	"go.uber.org/zap"
 )
 
+// Dump returns a RoundTripper which prints out the request & response object
+func Dump() RoundTripware {
+	return func(l *zap.Logger, next Handler) Handler {
+		return func(req *http.Request) (*http.Response, error) {
+			dumpRequest(req)
+			resp, err := next(req)
+			dumpResponse(req, resp)
+			return resp, err
+		}
+	}
+}
+
 // DumpRequest returns a RoundTripper which prints out the request object
 func DumpRequest() RoundTripware {
 	return func(l *zap.Logger, next Handler) Handler {
 		return func(req *http.Request) (*http.Response, error) {
-			if req.Header != nil && req.Header.Get("Content-Type") != "" {
-				var body string
-				if req.Body, body = readBodyPretty(req.Header.Get("Content-Type"), req.Body); body != "" {
-					fmt.Printf("Request %s:\n%s\n", req.URL, body)
-				}
-			}
+			dumpRequest(req)
 			return next(req)
 		}
 	}
@@ -27,13 +34,26 @@ func DumpResponse() RoundTripware {
 	return func(l *zap.Logger, next Handler) Handler {
 		return func(req *http.Request) (*http.Response, error) {
 			resp, err := next(req)
-			if resp.Header != nil && resp.Header.Get("Content-Type") != "" {
-				var body string
-				if resp.Body, body = readBodyPretty(resp.Header.Get("Content-Type"), resp.Body); body != "" {
-					fmt.Printf("Response %s:\n%s\n", req.URL, body)
-				}
-			}
+			dumpResponse(req, resp)
 			return resp, err
+		}
+	}
+}
+
+func dumpRequest(req *http.Request) {
+	if req.Header != nil && req.Header.Get("Content-Type") != "" {
+		var body string
+		if req.Body, body = readBodyPretty(req.Header.Get("Content-Type"), req.Body); body != "" {
+			fmt.Printf("Request %s:\n%s\n", req.URL, body)
+		}
+	}
+}
+
+func dumpResponse(req *http.Request, resp *http.Response) {
+	if resp.Header != nil && resp.Header.Get("Content-Type") != "" {
+		var body string
+		if resp.Body, body = readBodyPretty(resp.Header.Get("Content-Type"), resp.Body); body != "" {
+			fmt.Printf("Response %s:\n%s\n", req.URL, body)
 		}
 	}
 }
