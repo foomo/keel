36c74ef9fa68bd60609a9d98e952d6ecd1a2361e
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2022-03-17 09:36:55 +0100

    docs: update example

--- example/healthz/main.go
+++ example/healthz/main.go
@@ -10,24 +10,41 @@ import (
 	"github.com/foomo/keel/example/healthz/handler"
 )
 
+// See k8s for probe documentation
+// https://kubernetes.io/docs/concepts/workloads/pods/pod-lifecycle/#types-of-probe
 func main() {
 	// you can override the below config by settings env vars
 	_ = os.Setenv("SERVICE_HEALTHZ_ENABLED", "true")
 
 	svr := keel.NewServer(
 		// add probes service listening on :9400
-		// allows you to use probes for health checks in cluster: GET :9400/healthz
+		// allows you to use probes for health checks in cluster:
+		//	GET :9400/healthz
+		//  GET :9400/healthz/readiness
+		//  GET :9400/healthz/liveness
+		//  GET :9400/healthz/startup
 		keel.WithHTTPHealthzService(true),
 	)
 
 	l := svr.Logger()
 
-	// Add probe handlers
-	svr.AddAlwaysHealthzers(handler.New(l, "always"))
-	svr.AddStartupHealthzers(handler.New(l, "startup"))
-	svr.AddLivenessHealthzers(handler.New(l, "liveness"))
-	svr.AddReadinessHealthzers(handler.New(l, "readiness"))
+	// Add a probe that always be called
+	ah := handler.New(l, "always")
+	svr.AddAlwaysHealthzers(ah)
 
+	// Add a probe only on startup
+	sh := handler.New(l, "startup")
+	svr.AddStartupHealthzers(sh)
+
+	// Add a probe only on liveness
+	lh := handler.New(l, "liveness")
+	svr.AddLivenessHealthzers(lh)
+
+	// Add a probe only on readiness
+	rh := handler.New(l, "readiness")
+	svr.AddReadinessHealthzers(rh)
+
+	// add inline probe e.g. in case you start go routines
 	svr.AddAlwaysHealthzers(keel.NewHealthzerFn(func(ctx context.Context) error {
 		l.Info("healther fn")
 		return nil
@@ -40,14 +57,16 @@ func main() {
 		_, _ = w.Write([]byte("OK"))
 	})
 
+	// long taking initialization
 	l.Info("doing some initialization")
-	time.Sleep(10 * time.Second)
+	time.Sleep(30 * time.Second)
 	l.Info("initialization done")
 
-	// TODO wait for services to be started
+	// add services
 	svr.AddService(
 		keel.NewServiceHTTP(l, "demo", "localhost:8080", svs),
 	)
 
+	// start serer
 	svr.Run()
 }
