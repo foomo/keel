e72e832af3799072e42b69ad42a876a1686d4227
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2022-05-07 18:13:08 +0200

    feat: add logger

--- integration/temporal/service.go
+++ integration/temporal/service.go
@@ -4,15 +4,24 @@ import (
 	"context"
 
 	"go.temporal.io/sdk/worker"
+	"go.uber.org/zap"
+
+	"github.com/foomo/keel/log"
 )
 
 type service struct {
+	l    *zap.Logger
 	w    worker.Worker
 	name string
 }
 
-func NewService(name string, w worker.Worker) *service {
-	return &service{name: name, w: w}
+func NewService(l *zap.Logger, name string, w worker.Worker) *service {
+	if l == nil {
+		l = log.Logger()
+	}
+	// enrich the log
+	l = log.WithHTTPServerName(l, name)
+	return &service{l: l, name: name, w: w}
 }
 
 func (s *service) Name() string {
@@ -20,10 +29,12 @@ func (s *service) Name() string {
 }
 
 func (s *service) Start(ctx context.Context) error {
+	s.l.Info("starting temporal worker")
 	return s.w.Start()
 }
 
 func (s *service) Close(ctx context.Context) error {
+	s.l.Info("stopping temporal worker")
 	s.w.Stop()
 	return nil
 }
