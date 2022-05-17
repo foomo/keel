af2b13e4a570233cd96c9ba51e113e19d1951e3b
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2022-05-18 00:28:03 +0200

    feat: add timeout

--- config/remote.go
+++ config/remote.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"time"
+
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 )
@@ -10,8 +12,14 @@ func WithRemoteConfig(c *viper.Viper, provider, endpoint, path string) error {
 		return errors.Wrap(err, "failed to add remote provider")
 	}
 
-	if err := c.ReadRemoteConfig(); err != nil {
-		return errors.Wrap(err, "failed to read remote config")
+	var i int
+	for {
+		if err := c.ReadRemoteConfig(); err != nil && i < 5 {
+			time.Sleep(time.Second)
+			i++
+		} else if err != nil {
+			return errors.Wrap(err, "failed to read remote config")
+		}
 	}
 
 	if err := c.WatchRemoteConfigOnChannel(); err != nil {
