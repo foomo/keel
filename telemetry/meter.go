f40108293eb38d92e7350a04dc040c0bc0be03fb
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2022-06-20 12:15:40 +0200

    feat: increase historgram

--- telemetry/meter.go
+++ telemetry/meter.go
@@ -23,7 +23,7 @@ import (
 
 var (
 	// DefaultHistogramBuckets units are selected for metrics in "seconds" unit
-	DefaultHistogramBuckets = []float64{.001, .005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10, 25, 60}
+	DefaultHistogramBuckets = []float64{.001, .005, .01, .025, .05, .1, .25, .5, 1, 2.5, 5, 10, 25, 60, 120, 300, 600}
 )
 
 func Meter() metric.Meter {
