7728908f8e094375b9ba9b86510e87d115895a74
Ognjen <ognjen.vladisavljevic@bestbytes.com>
2022-09-28 13:59:46 +0200

    feat: retry roundtripware

--- example/go.mod
+++ example/go.mod
@@ -20,6 +20,7 @@ require (
 	cloud.google.com/go/compute v1.6.1 // indirect
 	cloud.google.com/go/firestore v1.6.1 // indirect
 	github.com/armon/go-metrics v0.3.10 // indirect
+	github.com/avast/retry-go v3.0.0+incompatible // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
---
+++ net/http/roundtripware/retry.go
@@ -0,0 +1,107 @@
+package roundtripware
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"time"
+
+	"github.com/avast/retry-go"
+	"go.uber.org/zap"
+)
+
+type (
+	RetryOptions struct {
+		Handler      RetryHandler
+		retryOptions []retry.Option
+	}
+	RetryHandler func(*http.Response) error
+	RetryOption  func(*RetryOptions)
+)
+
+func GetDefaultRetryOptions() RetryOptions {
+	return RetryOptions{
+		Handler: func(resp *http.Response) error {
+			if resp.StatusCode != http.StatusOK {
+				return errors.New("status code not ok")
+			}
+			return nil
+		},
+	}
+}
+
+func RetryWithAttempts(v uint) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.Attempts(v))
+	}
+}
+
+func RetryWithContext(ctx context.Context) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.Context(ctx))
+	}
+}
+
+func RetryWithDelay(delay time.Duration) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.Delay(delay))
+	}
+}
+
+func RetryWithMaxDelay(maxDelay time.Duration) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.MaxDelay(maxDelay))
+	}
+}
+
+func RetryWithDelayType(delayType retry.DelayTypeFunc) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.DelayType(delayType))
+	}
+}
+
+func RetryWithOnRetry(onRetry retry.OnRetryFunc) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.OnRetry(onRetry))
+	}
+}
+
+func RetryWithLastErrorOnly(lastErrorOnly bool) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.LastErrorOnly(lastErrorOnly))
+	}
+}
+
+func RetryWithRetryIf(retryIf retry.RetryIfFunc) RetryOption {
+	return func(o *RetryOptions) {
+		o.retryOptions = append(o.retryOptions, retry.RetryIf(retryIf))
+	}
+}
+
+// Retry returns a RoundTripper which retries failed requests
+func Retry(opts ...RetryOption) RoundTripware {
+	o := GetDefaultRetryOptions()
+	for _, opt := range opts {
+		if opt != nil {
+			opt(&o)
+		}
+	}
+	return func(l *zap.Logger, next Handler) Handler {
+		return func(req *http.Request) (*http.Response, error) {
+			var resp *http.Response
+			err := retry.Do(func() error {
+				value, err := next(req)
+				defer value.Body.Close()
+				if err != nil {
+					return err
+				} else if err := o.Handler(value); err != nil {
+					return err
+				} else {
+					resp = value
+					return nil
+				}
+			}, o.retryOptions...)
+			return resp, err
+		}
+	}
+}
