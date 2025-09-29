e93f6fb9df56786fe662cf0c4d123ece896fb4c8
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2025-09-29 21:48:58 +0200

    feat: deprecate keelgotsrpc

--- net/gotsrpc/decode.go
+++ net/gotsrpc/decode.go
@@ -3,6 +3,7 @@ package keelgotsrpc
 import "github.com/mitchellh/mapstructure"
 
 // Decode decodes the custom data into the given pointer to a map or struct.
+// Deprecated: use mapstructure.Decode instead.
 func Decode(data interface{}, v interface{}) error {
 	return mapstructure.Decode(data, v)
 }
