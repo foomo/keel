bb868e36f4a00c0e6513c5a83177ec55c887c303
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-02-17 17:29:46 +0100

    feat: enable modernize linter newexpr

--- net/gotsrpc/decode.go
+++
@@ -1,9 +0,0 @@
-package keelgotsrpc
-
-import "github.com/mitchellh/mapstructure"
-
-// Decode decodes the custom data into the given pointer to a map or struct.
-// Deprecated: use mapstructure.Decode instead.
-func Decode(data interface{}, v interface{}) error {
-	return mapstructure.Decode(data, v)
-}
